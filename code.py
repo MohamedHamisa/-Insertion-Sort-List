87dbcd77dffb53ce88c78fb92dc3a0b6932f3a0f
Mohamed Hamisa <108889205+MohamedHamisa@users.noreply.github.com>
2023-03-01 19:14:39 +0200

    Update code.py

--- code.py
+++ code.py
@@ -16,5 +16,8 @@ class Solution:
         
         return dummy.next
 
+    '''
+    time complexity is O(n^2), as Insertion sort should be: we take O(n) loops with O(n) loop inside. Space complexity is O(1), because what we do is only change connections between our nodes, and do not create new data.
+    '''
 
         
